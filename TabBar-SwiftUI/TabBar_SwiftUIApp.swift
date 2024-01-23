2d3fe8b7ae790803b34de9a93251e2f598bcbf75
elifogreniyor <elfogreniyor@gmail.com>
2024-01-23 20:33:55 +0300

    Initial Commit

---
+++ TabBar-SwiftUI/TabBar_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabBar_SwiftUIApp.swift
+//  TabBar-SwiftUI
+//
+//  Created by Elif Pamuk on 23.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct TabBar_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
